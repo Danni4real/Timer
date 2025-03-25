cfb3575c4f1bc001fdd5e2c1124481f7e2bf4d36
Dan Ni <61080530+Danni4real@users.noreply.github.com>
2025-03-25 16:58:26 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,52 @@
+#include "Timer.h"
+
+void loop(Timer *timer) {
+    for (int i = 0; i < 10000; i++) {
+        std::cerr << i << std::endl;
+
+        timer->set_timeout(1);
+        timer->set_callback([]() {
+            std::cout << "Timer expired! Executing callback..." << std::endl;
+        });
+        timer->start();
+        timer->stop();
+        timer->timed_out();
+    }
+}
+
+int main() {
+    Timer timer;
+
+    std::thread t1(loop, &timer);
+    std::thread t2(loop, &timer);
+    std::thread t3(loop, &timer);
+    
+    t1.join();
+    t2.join();
+    t3.join();
+
+    timer.set_callback([]() {
+        std::cout << "Timer expired! Executing callback..." << std::endl;
+    });
+
+    std::cout << "test 1:" << std::endl;
+    timer.set_timeout(2);
+    timer.start();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    timer.stop();
+
+    std::cout << "\ntest 2:" << std::endl;
+    timer.set_timeout(1);
+    timer.start();
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    timer.stop();
+
+    std::cout << "\ntest 3:" << std::endl;
+    timer.set_timeout(1);
+    timer.start();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    timer.stop();
+
+    std::cout << "\nDestory timer:" << std::endl;
+    return 0;
+}
