24ce9bc598ce7fdb365eeb513b1a553ea35b0dd6
Dan Ni <61080530+Danni4real@users.noreply.github.com>
2025-03-25 16:57:58 +0800

    Create Timer.h

---
+++ Timer.h
@@ -0,0 +1,53 @@
+//
+// Created by dan on 25-3-24.
+//
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <mutex>
+#include <chrono>
+#include <thread>
+#include <iostream>
+#include <functional>
+#include <condition_variable>
+
+class Timer {
+public:
+    Timer();
+    ~Timer();
+
+    bool set_timeout(int seconds);
+    bool set_callback(const std::function<void()> &callback);
+
+    void start();
+    void stop();
+
+    bool timed_out();
+
+private:
+    int timeout_sec{0};
+    bool running_{false};
+    bool timed_out_{false};
+    bool cmd_executed{false}; // start or stop cmd
+    bool quit_thread_{false}; // only used by destructor
+    std::function<void()> callback_{nullptr};
+
+    std::thread thread_;
+    std::mutex timer_mtx_; // ensure mutual exclusion between caller thread and timer thread
+    std::mutex caller_mtx_; // ensure mutual exclusion between multiple caller threads
+    std::condition_variable cv_;
+
+    bool set_timeout_private(int seconds);
+    bool set_callback_private(const std::function<void()> &callback);
+
+    void start_private();
+    void stop_private();
+
+    bool timed_out_private();
+
+    void run();
+};
+
+#endif //TIMER_H
+
